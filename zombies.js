76a2aa98b04d88ceae86fe1266b558fe66a8c05d
leticia1804 <89095951+leticia1804@users.noreply.github.com>
2021-12-16 18:42:45 -0300

    Update zombies.js

--- zombies.js
+++ zombies.js
@@ -1,23 +1,21 @@
 class Zombies{
     constructor(x,y){
-        this.positionX = 0;
-        this.positionY = 0;
+        this.positionX = x;
+        this.positionY = y;
 
-        this.width = 5;
-        this.height = 5;
-
-
+        this.sprite = createSprite(x,y);
         this.npcz1_Anm = loadAnimation("../imagens/Zombie_L1.png", 
         "../imagens/Zombie_L2.png", "../imagens/Zombie_L3.png", 
         "../imagens/Zombie_L4.png");
 
-        //this.npcz1_Anm.scale = 0.006
+        this.sprite.addAnimation("NPC animacao",this.npcz1_Anm)
+       
+        this.sprite.scale = 0.004;
+        this.sprite.velocityX = -1;
+        this.sprite.velocityY = 0.5;
 
         
     }
 
-    display(){
-        //imageMode(CENTER);
-        animation(this.npcz1_Anm, this.positionX, this.positionY, this.width, this.height);
-    }
+    
 }
